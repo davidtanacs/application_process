e19445e684969c8a8414362c8e8af818ea13d02b
davidtanacs <tanacsdavid90@gmail.com>
2017-05-17 20:37:04 +0200

    Made the app working continuously with exit(0)

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import querys
+import sys
 
 
 def main():
-    choice = querys.show_menu()
-    conn = querys.connect_to_db()
-    if choice == "1":
-        querys.show_mentors_name(conn)
-    elif choice == "2":
-        querys.show_mentors_nick_miskolc(conn)
-    elif choice == "3":
-        querys.show_carols_data(conn)
-    elif choice == "4":
-        querys.hat_owners_data(conn)
-    elif choice == "5":
-        querys.insert_markus_datas(conn)
-    elif choice == "6":
-        querys.update_jemimas_data(conn)
-    elif choice == "7":
-        querys.del_arsenio_and_friend(conn)
+    choice = 8
+    while choice > 0 and choice <= 8:
+        choice = querys.show_menu()
+        conn = querys.connect_to_db()
+        if choice == 1:
+            querys.show_mentors_name(conn)
+        elif choice == 2:
+            querys.show_mentors_nick_miskolc(conn)
+        elif choice == 3:
+            querys.show_carols_data(conn)
+        elif choice == 4:
+            querys.hat_owners_data(conn)
+        elif choice == 5:
+            querys.insert_markus_datas(conn)
+        elif choice == 6:
+            querys.update_jemimas_data(conn)
+        elif choice == 7:
+            querys.del_arsenio_and_friend(conn)
+        elif choice == 0:
+            sys.exit
+    choice = int(8)
 
 if __name__ == '__main__':
     main()
--- querys.py
+++ querys.py
@@ -3,14 +3,15 @@ import psycopg2
 
 def show_menu():
     choice = input("""
-            Press 1 to Mentors' name\n
-            Press 2 to Mentors' nickname at Miskolc\n
-            Press 3 to Carol's full name and phone number\n
-            Press 4 to hat-owner's full name and phone number\n
-            Press 5 to Markus Schaffarzyk's datas\n
+            Press 1 to see Mentors' name\n
+            Press 2 to see Mentors' nickname at Miskolc\n
+            Press 3 to see Carol's full name and phone number\n
+            Press 4 to see hat-owner's full name and phone number\n
+            Press 5 to see Markus Schaffarzyk's datas\n
             Press 6 to update Jemima Foreman's phone number and see it\n
-            Press 7 to delete Arsenio and his friend from the database""")
-    return choice
+            Press 7 to delete Arsenio and his friend from the database
+            Or 0 to quit""")
+    return int(choice)
 
 
 def connect_to_db():
